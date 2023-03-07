154cb50499deabf6e2439313af7742646c563a84
“4larin” <folarin.tunji98@gmail.com>
2023-03-07 19:38:26 +0100

    root

--- .github/grv/index.js
+++ .github/grv/index.js
@@ -4,20 +4,38 @@ const fs = require('fs')
 const YAML = require('yaml')
 const github = require('@actions/github');
 const { context } = require('@actions/github')
-
-
-
-const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+const GITHUB_TOKEN = "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";
 const octokit = github.getOctokit(GITHUB_TOKEN);
- 
+
+
 const { pull_request } = context.payload;
- 
-async function run() {
-  await octokit.rest.issues.createComment({
-    ...context.repo,
-    issue_number: pull_request.number,
-    body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
-  });
+const privateFilePath = '../../configuration/private'
+const publicFilePath = '../../configuration/public'
+const utf8Encoding = 'utf-8'
+let errorCount = 0
+
+
+const runAction = async () => {
+  try {
+
+    console.log('javascript works')
+
+    core.info('verify-domain-name-uniqueness-action: starting')
+
+
+    if (errorCount > 0) throw new Error(`${errorCount} Opens-search yaml configuration errors: Duplicate domain names`)
+
+  } catch (error) {
+    core.setFailed(error)
+  }
 }
- 
-run();
+
+
+
+
+
+runAction()
+
+
+
+
